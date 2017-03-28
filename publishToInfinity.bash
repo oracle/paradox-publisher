d76c287b3245f35bcd44c7dc93e3a813bf2a8b0d
gastong <gregory.gaston@webtrends.com>
2017-03-28 10:13:43 -0700

    Refactor. Added dscsip, dcsaut. wt.cg_n. Fixed co_f.

--- publishToInfinity.bash
+++ publishToInfinity.bash
@@ -1,12 +1,11 @@
 #!/bin/bash
 // 2>/dev/null; /usr/bin/env groovy "$0" "$@"; exit $?;
 
-// Description:  This script takes results from Webtrends/webtesting and imports them into a Infinity
+// Description:  This script takes results from Webtrends/Paradox-testing and imports them into Infinity
 
-autoUrl='http://pdxauto03.englab.webtrends.corp:8080/webtesting/'
-scsUrl='http://scs.webtrends.com/dcsqm14ha10000s142fw1a8lw_6m3f/dcs.gif'
-def userAgent = 'paradoxPublisherRestClient'
-def publisherVersion = '0.1'
+autoUrl = 'http://pdxauto03.englab.webtrends.corp:8080/webtesting/'
+scsUrl = 'http://scs.webtrends.com/dcsqm14ha10000s142fw1a8lw_6m3f/dcs.gif'
+publisherVersion = '0.1'
 
 // Job parameters
 @Grab('org.codehaus.groovy.modules.http-builder:http-builder:0.7.+' )
@@ -14,8 +13,9 @@ import groovyx.net.http.*
 import groovy.json.*
 assert (assemblyName = args[0]) && 'assemblyName is required'
 assert (executionGuid = args[1]) && 'executionGuid is required'
-def isDebug = args[2]?.toBoolean()
+isDebug = args[2]?.toBoolean()
 
+def cof = executionGuid + (isDebug ? new Random().nextInt().toString() : '')
 auto = new RESTClient(autoUrl, 'application/json')
 scs = new RESTClient().with { parser.'image/gif' = parser.defaultParser; it }
 
@@ -23,37 +23,39 @@ println "Fetching results from ${auto.defaultURI}results/$assemblyName/$executio
 assert (results = auto.get(path: "results/$assemblyName/$executionGuid").data)
 println "Results = $results"
 
-// Send hit with Session Information
-def sessionEvent = [
-    dcsref: 'Paradox Publisher',
+def sendEvent(Map event) {
+    event << [
+        dcsref: '()aradox',
+        publisherVersion: publisherVersion,
+        isDebug: "$isDebug",
+    ]
+    event = event.findAll { it.value }.collectEntries { [it.key, URLEncoder.encode(it.value, 'UTF-8')] }
+    def queryString = event.collect { "$it.key=$it.value" }.join('&')
+    scs.get(uri: "$scsUrl?$queryString", headers: ['User-Agent': 'paradoxRestClient'])
+}
+
+//Send Test Suite
+sendEvent(
+    'wt.co_f': cof,
     dcsuri: assemblyName,
-    publisherVersion: publisherVersion,
-    isDebug: "$isDebug",
     suiteName: assemblyName,
-    co_f: executionGuid + isDebug ? new Random().nextInt().toString() : '',
     environment: results.environment,
     date: results.date,
     time: results.time,
     commandLine: results.commandline,
-    //TODO: machineName: dcsSip ,
-    //TODO: userName: dcsAut ,
-    ].findAll{ it.value }.collectEntries { [it.key, URLEncoder.encode(it.value, 'UTF-8')] }
+    dscsip: InetAddress.localHost.hostName,
+    dcsaut: System.getProperty('user.name')
+    )
 
-scs.get(uri:"$scsUrl?${sessionEvent.collect{"$it.key=$it.value"}.join('&')}",headers:['User-Agent':userAgent])
-
-// Send hit with Test Information
+//Send Test Results
 for(test in results.tests) {
-    def testEvent = [
-        dcsref: 'Paradox Publisher',
-        dcsuri: test.name,
-        publisherVersion: publisherVersion,
-        isDebug: "$isDebug",
+    sendEvent(
+        'wt.co_f': cof,
+        dcsuri: test.name.replaceAll('\\.', '/'),
         testName: test.name,
-        labels: test.labels.join(';') ,
+        'wt.cg_n': test.labels.join(';'),
         state: test.state,
         performance: test.performance.toString(),
         defect: test.defect,
-    ].findAll{ it.value }.collectEntries { [it.key, URLEncoder.encode(it.value, 'UTF-8')] }
-
-    scs.get(uri:"$scsUrl?${testEvent.collect{"$it.key=$it.value"}.join('&')}",headers:['User-Agent':userAgent])
+    )
 }
